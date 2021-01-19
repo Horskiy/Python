bc828b70c5b25e4fa7fbd4d84f8c855627b0935e
Horskiy <horskiy@gmail.com>
2021-01-19 22:10:54 +0300

    Add cals methods.

---
+++ Methods.py
@@ -0,0 +1,14 @@
+def sum1(a, b, c=3):
+    return a + b + c
+
+
+def multiplication(a, b):
+    return a * b
+
+
+def division(a, b):
+    return a / b
+
+
+def subtraction(a, b):
+    return a - b
--- main.py
+++ main.py
@@ -1,4 +1,17 @@
+from Methods import *
+
+
 print("Hello world!")
+
+print(sum1(3, 4))
+
+print(subtraction(3, 4))
+
+print(multiplication(3, 4))
+
+print(division(3, 4))
+
+print(subtraction(3, 4))
 
 
 
